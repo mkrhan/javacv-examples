fe483cfdff1133757c31c78463318fe5a718e84e
jarek.listy@gmail.com <jarek.listy@gmail.com@62ea4864-4701-0dd2-2ed8-cd8fe7c697b1>
2011-10-07 20:30:59 +0000

    Add documentation and optional parameters (with default values).

--- trunk/OpenCV2_Cookbook/src/opencv2_cookbook/OpenCVUtils.scala
+++ trunk/OpenCV2_Cookbook/src/opencv2_cookbook/OpenCVUtils.scala
@@ -85,11 +85,20 @@ object OpenCVUtils {
 
     /**
      * Load an image and show in a CanvasFrame. If image cannot be loaded the application will exit with code 1.
+     * @param flags Flags specifying the color type of a loaded image:
+     * <ul>
+     *     <li> {@code >0} Return a 3-channel color image</li>
+     *     <li> {@code =0} Return a grayscale image</li>
+     *     <li> {@code <0} Return the loaded image as is. Note that in the current implementation
+     *     the alpha channel, if any, is stripped from the output image. For example, a 4-channel
+     *     RGBA image is loaded as RGB if flags {@code >} 0.</li>
+     * </ul>
+     * Default is grayscale.
      * @return loaded image
      */
-    def loadMatAndShowOrExit(file: File): CvMat = {
+    def loadMatAndShowOrExit(file: File, flags: Int = CV_LOAD_IMAGE_GRAYSCALE): CvMat = {
         // Read input image
-        val image = loadMatOrExit(file)
+        val image = loadMatOrExit(file, flags)
         show(image, file.getName)
         image
     }
@@ -97,11 +106,20 @@ object OpenCVUtils {
 
     /**
      * Load an image. If image cannot be loaded the application will exit with code 1.
+     * @param flags Flags specifying the color type of a loaded image:
+     * <ul>
+     *     <li> {@code >0} Return a 3-channel color image</li>
+     *     <li> {@code =0} Return a grayscale image</li>
+     *     <li> {@code <0} Return the loaded image as is. Note that in the current implementation
+     *     the alpha channel, if any, is stripped from the output image. For example, a 4-channel
+     *     RGBA image is loaded as RGB if flags {@code >} 0.</li>
+     * </ul>
+     * Default is grayscale.
      * @return loaded image
      */
-    def loadMatOrExit(file: File): CvMat = {
+    def loadMatOrExit(file: File, flags: Int = CV_LOAD_IMAGE_GRAYSCALE): CvMat = {
         // Read input image
-        val image = cvLoadImageM(file.getAbsolutePath, 0)
+        val image = cvLoadImageM(file.getAbsolutePath, flags)
         if (image == null) {
             println("Couldn't load image: " + file.getAbsolutePath)
             sys.exit(1)
